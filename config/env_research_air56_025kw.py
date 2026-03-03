3e390f672e0211c0c391fb4a052d25468ae77df0
Лисов <11lisov11@gmail.com>
2026-03-03 03:01:23 +0300

    Update AIR56 AI eval defaults from tuned start_stop search

--- config/env_research_air56_025kw.py
+++ config/env_research_air56_025kw.py
@@ -91,30 +91,30 @@ ai_drift_ranges = {
 # Expected checkpoint location after training:
 #   python -m mic_ai.ai.train_ai_id_ref config/env_research_air56_025kw.py ...
 ai_eval_checkpoint_path = "outputs/ai_id_ref/checkpoints/env_research_air56_025kw/best_actor.pth"
-ai_eval_id_ref_alpha = 0.6916041739118715
-ai_eval_delta_id_max = 0.41175616659850595
+ai_eval_id_ref_alpha = 0.8077602446952543
+ai_eval_delta_id_max = 0.1979319823875324
 ai_eval_id_ref_relative = True
 ai_eval_id_ref_allow_positive_delta = True
-ai_eval_id_ref_gate_speed_tol_rel = 0.12802819602776283
-ai_eval_id_ref_gate_min_scale = 0.10854865567909161
-ai_eval_id_ref_gate_exponent = 1.2112607240172235
+ai_eval_id_ref_gate_speed_tol_rel = 0.05977966855238324
+ai_eval_id_ref_gate_min_scale = 0.047560004818945
+ai_eval_id_ref_gate_exponent = 1.2921867188665694
 
 ai_eval_supervisor_enabled = True
 ai_eval_sup_objective = "specific_power"
-ai_eval_sup_speed_tol_rel = 0.07682193507978134
+ai_eval_sup_speed_tol_rel = 0.06384288845942572
 ai_eval_sup_speed_tol_abs = 0.0
 ai_eval_sup_omega_min = 0.07740003039871146
-ai_eval_sup_update = 11
-ai_eval_sup_dither = 0.004179110345105394
-ai_eval_sup_step = 0.010135882917319504
-ai_eval_sup_bias_max = 0.21136303877643553
+ai_eval_sup_update = 17
+ai_eval_sup_dither = 0.009461244984793124
+ai_eval_sup_step = 0.012567257092204
+ai_eval_sup_bias_max = 0.18047761179912863
 ai_eval_sup_shaft_eps = 10.0
 ai_eval_sup_reset_decay = 0.9886974417190302
 ai_eval_sup_idle_enable = True
-ai_eval_sup_idle_omega_min = 0.07740003039871146
-ai_eval_sup_idle_action = -0.6189698591906893
-ai_eval_sup_idle_exit_boost = 39
-ai_eval_sup_idle_exit_action = 0.817417657326223
-ai_eval_sup_idle_bias_decay = 0.9886974417190302
+ai_eval_sup_idle_omega_min = 0.04356872117797547
+ai_eval_sup_idle_action = -0.8054457650121533
+ai_eval_sup_idle_exit_boost = 12
+ai_eval_sup_idle_exit_action = 0.8230332443977089
+ai_eval_sup_idle_bias_decay = 0.9400017812223386
 
 __all__ = ["ENV"]
