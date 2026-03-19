b4370abb91887fe5321e2e5655dc4f37d1be027f
Лисов <11lisov11@gmail.com>
2026-03-19 23:38:13 +0300

    1

---
+++ outputs/tmp_air56_train_sup_rand001_20260319.py
@@ -0,0 +1,21 @@
+﻿from config.env_research_air56_025kw import *  # noqa: F401,F403
+
+# Temporary aligned training supervisor for AIR56 continuation research.
+ai_eval_supervisor_enabled = True
+ai_eval_sup_objective = "p_in"
+ai_eval_sup_speed_tol_rel = 0.07263920179946834
+ai_eval_sup_speed_tol_abs = 0.0
+ai_eval_sup_omega_min = 0.07740003039871146
+ai_eval_sup_update = 19
+ai_eval_sup_dither = 0.02326033594770624
+ai_eval_sup_step = 0.006299850310235484
+ai_eval_sup_bias_max = 0.16611584839328392
+ai_eval_sup_shaft_eps = 10.0
+ai_eval_sup_reset_decay = 0.9886974417190302
+ai_eval_sup_idle_enable = True
+ai_eval_sup_idle_omega_min = 0.05965620238838795
+ai_eval_sup_idle_action = -0.6718358726341831
+ai_eval_sup_idle_blend = 1.0
+ai_eval_sup_idle_exit_boost = 1
+ai_eval_sup_idle_exit_action = 0.9108384980085505
+ai_eval_sup_idle_bias_decay = 0.9404969031876687
---
+++ outputs/tmp_ao2_train_sup_microtight_20260318.py
@@ -0,0 +1,21 @@
+﻿from config.env_research_ao2_32_4_3kw import *  # noqa: F401,F403
+
+# Temporary aligned training supervisor for AO2 continuation research.
+ai_eval_supervisor_enabled = True
+ai_eval_sup_objective = "p_in"
+ai_eval_sup_speed_tol_rel = 0.1225
+ai_eval_sup_speed_tol_abs = 0.0
+ai_eval_sup_omega_min = 0.1
+ai_eval_sup_update = 26
+ai_eval_sup_dither = 0.0108
+ai_eval_sup_step = 0.0051
+ai_eval_sup_bias_max = 0.1415
+ai_eval_sup_shaft_eps = 10.0
+ai_eval_sup_reset_decay = 0.98
+ai_eval_sup_idle_enable = False
+ai_eval_sup_idle_omega_min = 0.05
+ai_eval_sup_idle_action = -0.25
+ai_eval_sup_idle_blend = 0.10
+ai_eval_sup_idle_exit_boost = 0
+ai_eval_sup_idle_exit_action = 0.90
+ai_eval_sup_idle_bias_decay = 0.98
